e75ab326c8aa9881c286f5d9203cc67f4abeaa45
ShariarShantoDev <iamshanto.dev@gmail.com>
2026-08-18 22:07:03 +0600

    update experience and education

--- src/app/components/education/education.component.ts
+++ src/app/components/education/education.component.ts
@@ -1,11 +1,12 @@
 import { Component, Input } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { PORTFOLIO_DATA } from '../../data/portfolio.config';
+import { FloatInDirective } from '../../directives/float-in.directive';
 
 @Component({
   selector: 'app-education',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, FloatInDirective],
   templateUrl: './education.component.html',
   styleUrl: './education.component.css'
 })
